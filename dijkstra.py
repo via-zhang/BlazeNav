133ea873aff21818a46b83be177468e09776e9a7
Jaden Delapaz <jaden.delapaz@gmail.com>
2025-04-18 22:25:14 -0400

    initial dijkstra's algorithm commit

---
+++ dijkstra.py
@@ -0,0 +1,18 @@
+# Dijkstra's Algorithm Implementation
+def Dijkstra(matrix, startingNodeLongitude, startingNodeLatitude):
+    matrixSize = len(matrix)
+    # Starting index
+    startingNode = matrix[startingNodeLongitude[startingNodeLatitude]]
+    # Distance map
+    distance = {
+
+    }
+    # Predecessor map
+    predecessor = {
+
+    }
+    # Current node map
+    visitedNodes = {
+        
+    }
+    
